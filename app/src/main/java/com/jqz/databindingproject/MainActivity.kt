52724de4e5db0278d517f9080c5a392072dac085
jinqianzhi <jinqianzhivvvip@126.com>
2021-01-15 16:14:29 +0800

    首次提交测试

---
+++ app/src/main/java/com/jqz/databindingproject/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.jqz.databindingproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.setPadding
+import androidx.databinding.DataBindingUtil
+import com.jqz.databindingproject.databinding.ActivityMainBinding
+import com.jqz.databindingproject.model.DataBean
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        binding.bean = DataBean("张三", 19, "男")
+    }
+}
---
+++ app/src/main/java/com/jqz/databindingproject/model/DataBean.kt
@@ -0,0 +1,13 @@
+package com.jqz.databindingproject.model
+
+/**
+ * author：jqz
+ * 创建时间：2021/1/14
+ * 包名：com.jqz.databindingproject.model
+ * TODO:
+ */
+data class DataBean(
+    val name: String? = "张三",
+    val age: Int? = 18,
+    val sex: String? = "男"
+)
