74311afbf0249da6cefc6c9aee5defdd1141ce53
jinqianzhi <jinqianzhivvvip@126.com>
2021-01-20 19:47:01 +0800

    devlop/v1.2.0第一次提交

--- app/src/main/java/com/jqz/databindingproject/MainActivity.kt
+++ app/src/main/java/com/jqz/databindingproject/MainActivity.kt
@@ -2,10 +2,7 @@ package com.jqz.databindingproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.setPadding
-import androidx.databinding.DataBindingUtil
 import com.jqz.databindingproject.databinding.ActivityMainBinding
-import com.jqz.databindingproject.model.DataBean
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
