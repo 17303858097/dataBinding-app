3b3c7c6191705fe9b53367f73636b06069e0bf38
jinqianzhi <jinqianzhivvvip@126.com>
2021-01-20 19:51:33 +0800

    第二次提交

--- app/src/main/java/com/jqz/databindingproject/model/DataBean.kt
+++ app/src/main/java/com/jqz/databindingproject/model/DataBean.kt
@@ -7,7 +7,7 @@ package com.jqz.databindingproject.model
  * TODO:
  */
 data class DataBean(
-    val name: String? = "张三",
+    val userName: String? = "张三",
     val age: Int? = 18,
-    val sex: String? = "男"
+    val userSex: String? = "男"
 )
